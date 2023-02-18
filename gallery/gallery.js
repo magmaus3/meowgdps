9c0fccf521efefc4ed7130c8b0fd8095ee45f623
MeFinity <72972952+MeFinity@users.noreply.github.com>
2023-02-18 15:46:52 +0700

    Add files via upload

---
+++ gallery/gallery.js
@@ -0,0 +1,21 @@
+fetch(`https://e926.net/posts.json?tags=gdps2point2`, {})
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('latest-post');
+    if (data.posts.length === 0) {
+      const text = document.createElement('p');
+      text.innerText = "Looks like we haven't made anything";
+      container.appendChild(text);
+    } else {
+    for (let i = 0; i < data.posts.length; i++) {
+      const post = data.posts[i];
+      const img = document.createElement('img');
+      const link = document.createElement('a');
+      link.href = `https://e926.net/posts/${post.id}`;
+      img.src = post.file.url;
+      img.classList.add('gallery');
+      link.appendChild(img);
+      container.appendChild(link);
+    }
+  }
+  })
