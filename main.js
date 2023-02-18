5069b4d8d939898c5eb008555f7cc9918339a8ac
MeFinity <72972952+MeFinity@users.noreply.github.com>
2023-02-18 15:39:40 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-fetch(`https://e926.net/posts.json?tags=geometry_dash&limit=5`, {
+fetch(`https://e926.net/posts.json?tags=gdps2point2&limit=5`, {
   })
   .then(response => response.json())
   .then(data => {
     const container = document.getElementById('latest-post');
+    if (data.posts.length === 0) {
+      const text = document.createElement('p');
+      text.innerText = "Looks like we haven't made anything";
+      container.appendChild(text);
+    } else {
     for (let i = 0; i < data.posts.length; i++) {
       const img = document.createElement('img');
       img.src = data.posts[i].file.url;
       img.classList.add('gallery');
       container.appendChild(img);
     }
+  }
   })
