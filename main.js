0476a2fdb7c4ee067032f3e7db82de921cf0bb6b
MeFinity <72972952+MeFinity@users.noreply.github.com>
2023-03-09 19:23:05 +0700

    burger

--- main.js
+++ main.js
@@ -16,3 +16,16 @@ fetch(`https://e926.net/posts.json?tags=user:GDPS2point2&limit=5`, {
     }
   }
   })
+  function joke() {
+    fetch ('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=religious,political,racist,sexist')
+    .then(response => response.json())
+    .then(data => {
+      if (data.joke) {
+      const text = document.getElementById('joke')
+      text.innerText = data.joke;
+      } else {
+        const text = document.getElementById('joke')
+        text.innerText = data.setup + "\n" + data.delivery;
+      }
+    })
+  }
