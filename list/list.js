2cd3f7c82b238232c712a5c6c33f9ec116640384
MeFinity <72972952+MeFinity@users.noreply.github.com>
2023-03-11 21:16:48 +0700

    burger

--- list/list.js
+++ list/list.js
@@ -2,21 +2,29 @@ function trello() {
   fetch('https://api.trello.com/1/boards/0yEIn65R/cards')
     .then(response => response.json())
     .then(data => {
-      const cardList = document.getElementById('list');
-      cardList.innerHTML = '';
+      const not = document.getElementById('not');
+      const wip = document.getElementById('wip');
+      const done = document.getElementById('done');
+      not.innerHTML = '';
+      wip.innerHTML = '';
+      done.innerHTML = '';
       data.forEach(card => {
         const cardName = card.name;
-        const hasGreenLabel = card.labels.some(label => label.color === 'green');
-        const hasRedLabel = card.labels.some(label => label.color === 'red');
+        const isNOT = card.idList === '63f9cb252e9373a93c2dbba5';
+        const isWIP = card.idList === '63f9cb46d4811ca511fcbb27';
+        const isDONE = card.idList === '63f9d3eb8e5bda6dce9c197e';        
 
-        if (hasGreenLabel) {
-          cardList.innerHTML += '<div class="card"><span class="circle green"></span>' + cardName + '</div>';
-        } else if (hasRedLabel) {
-          cardList.innerHTML += '<div class="card"><span class="circle red"></span>' + cardName + '</div>';
+        if (isNOT) {
+          not.innerHTML += '<div class="card"><span class="circle red"></span>' + cardName + '</div>';
+        }
+        if (isWIP) {
+          wip.innerHTML += '<div class="card"><span class="piw"></span>' + cardName + '</div>';
+        }
+        if (isDONE) {
+          done.innerHTML += '<div class="card"><span class="circle green"></span>' + cardName + '</div>';
         }
       });
     });
 }
 
 trello();
-window.onload = trello;
