f73782fd96690d231ceadaf3b38f1c43c6d6a7a5
MeFinity <72972952+MeFinity@users.noreply.github.com>
2023-03-11 11:18:06 +0700

    Create this to make button functional

---
+++ credits/meow/meow.js
@@ -0,0 +1,27 @@
+function back() {
+    const container = document.getElementById('back');
+    container.innerText = "You'll never escape!";
+}
+
+function femboys() {
+    fetch('https://api.catboys.com/img')
+    .then(response => response.json())
+    .then(data => {
+    const container = document.getElementById('catboy');
+    const img = document.createElement('img');
+    img.src = data.url;
+    container.appendChild(img);
+    })
+}
+function notfemboys() {
+    const url = 'https://nekos.moe/api/v1/random/image?nsfw=false&count=1';
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('catgirl');
+        const img = document.createElement('img');
+        img.src = `https://nekos.moe/image/${data.images[0].id}`;
+        container.appendChild(img);
+    })
+    .catch(error => console.error('An error has occurred', error));
+}
