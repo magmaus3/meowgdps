a203dde7975d9c5e6817456c9ae01bfed817a265
MeFinity <72972952+MeFinity@users.noreply.github.com>
2023-03-30 15:11:32 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,67 @@
-fetch(`https://e926.net/posts.json?tags=user:GDPS2point2&limit=5`, {
+fetch(`https://e926.net/posts.json?tags=user:GDPS2point2&limit=5`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('oh no.');
+    }
+    return response.json();
   })
-  .then(response => response.json())
   .then(data => {
     const container = document.getElementById('latest-post');
-    if (data.posts.length === 0) {
-      const text = document.createElement('p');
-      text.innerText = "Looks like we haven't made anything";
-      container.appendChild(text);
-    } else {
     for (let i = 0; i < data.posts.length; i++) {
       const img = document.createElement('img');
       img.src = data.posts[i].file.url;
       img.classList.add('gallery');
+      img.addEventListener('load', () => img.classList.add('loaded'));
       container.appendChild(img);
     }
-  }
   })
-  function joke() {
-    fetch ('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=religious,political,racist,sexist')
+  .catch(error => {
+    const container = document.getElementById('latest-post');
+    const text = document.createElement('p');
+    text.innerText = "Whoops!, looks like e621 is down";
+    container.appendChild(text);
+  });
+
+function joke() {
+  const speechBubble = document.getElementById('joke');
+  if (speechBubble.classList.contains('visible')) {
+    speechBubble.classList.remove('visible');
+    speechBubble.innerText = "";
+    return;
+  }
+
+  fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=religious,political,racist,sexist')
     .then(response => response.json())
     .then(data => {
       if (data.joke) {
-      const text = document.getElementById('joke')
-      text.innerText = data.joke;
+        speechBubble.innerText = data.joke;
       } else {
-        const text = document.getElementById('joke')
-        text.innerText = data.setup + "\n" + data.delivery;
+        speechBubble.innerText = data.setup + "\n" + data.delivery;
       }
-    })
-  }
+      
+      const textLength = speechBubble.innerText.length;
+      switch (true) {
+        case (textLength > 95):
+          speechBubble.style.fontSize = '18px';
+          break;
+        case (textLength > 40):
+          speechBubble.style.fontSize = '22px';
+      }
+      
+      speechBubble.classList.add('visible');
+      
+      document.addEventListener('click', function(event) {
+        if (!speechBubble.contains(event.target)) {
+          speechBubble.classList.remove('visible');
+          speechBubble.innerText = "";
+        }
+      });
+    });
+}
+
+function openPopup() {
+  document.getElementById("creditsp").style.display = "block";
+}
+function closePopup() {
+  document.getElementById("creditsp").style.display = "none";
+}
