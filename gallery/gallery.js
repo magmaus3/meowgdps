7481322f3e4d96fce928fa85627cd2e921737c85
magmaus3 <magmaus3@disroot.org>
2023-04-20 12:27:30 +0200

    Show tumblr images in the gallery uses the saved response instead of fetching it directly, to avoid sharing the tumblr api key.

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -19,3 +19,26 @@ fetch(`https://e926.net/posts.json?tags=user:GDPS2point2`, {})
     }
   }
   })
+
+/* To avoid exposing the api key, a saved response is used instead
+ * 
+ * To generate:
+    # Executed in the repository's root
+    curl "https://api.tumblr.com/v2/blog/[[ BLOG NAME ]]/posts/photo?api_key=[[ YOUR API KEY ]]" > gallery/tumblr.json
+ */
+fetch(`/gallery/tumblr.json`, {})
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('latest-post');
+    let parser = new DOMParser();
+    let parsed
+    data.response.posts.forEach(post => {
+        parsed = parser.parseFromString(post.body, 'text/html')
+        Array.prototype.slice.call(parsed.getElementsByTagName("img")).forEach(img => {
+            let link = document.createElement("a")
+            link.setAttribute('href', post.post_url)
+            link.appendChild(img)
+            container.appendChild(link)
+        })
+    })
+})
